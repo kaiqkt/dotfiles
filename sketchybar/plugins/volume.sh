d5d7ddc5ac44e93a8d36917200da2bc2d17ace16
kaiqkt <kaiqkt@proton.me>
2026-06-13 15:54:36 -0300

    feat(sketchybar): scroll on volume item to adjust volume ±5%

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -1,11 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-# The volume_change event supplies a $INFO variable in which the current volume
-# percentage is passed to the script.
-
-if [ "$SENDER" = "volume_change" ]; then
-  VOLUME="$INFO"
-
+set_volume() {
+  local VOLUME="$1"
   case "$VOLUME" in
     [6-9][0-9]|100) ICON="󰕾" ;;
     [3-5][0-9])     ICON="󰖀" ;;
@@ -18,4 +14,17 @@ if [ "$SENDER" = "volume_change" ]; then
   else
     sketchybar --set "$NAME" drawing=off
   fi
+}
+
+if [ "$SENDER" = "volume_change" ]; then
+  set_volume "$INFO"
+
+elif [ "$SENDER" = "mouse.scrolled" ]; then
+  CURRENT=$(osascript -e 'output volume of (get volume settings)')
+  DELTA=$([ "$SCROLL_DELTA" -gt 0 ] && echo 5 || echo -5)
+  NEW=$(( CURRENT + DELTA ))
+  [ "$NEW" -gt 100 ] && NEW=100
+  [ "$NEW" -lt 0 ]   && NEW=0
+  osascript -e "set volume output volume $NEW"
+  set_volume "$NEW"
 fi

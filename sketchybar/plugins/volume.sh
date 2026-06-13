d6a13a4ad020bbe11abdfa0cb2109037122b8ac4
kaiqkt <kaiqkt@proton.me>
2026-06-13 16:08:17 -0300

    feat(sketchybar): toggle volume slider on icon click with animation

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -2,26 +2,38 @@
 
 source "$CONFIG_DIR/colors.sh"
 
-set_volume() {
+set_icons() {
   local VOLUME="$1"
-
   case "$VOLUME" in
     [6-9][0-9]|100) ICON="󰕾" ;;
     [3-5][0-9])     ICON="󰖀" ;;
     [1-9]|[1-2][0-9]) ICON="󰕿" ;;
     *)              ICON="󰖁" ;;
   esac
-
   sketchybar --set volume icon="$ICON" \
              --set volume_slider slider.percentage="$VOLUME"
 }
 
-if [ "$SENDER" = "volume_change" ]; then
-  set_volume "$INFO"
+toggle_slider() {
+  CURRENT_WIDTH=$(sketchybar --query volume_slider | python3 -c \
+    "import sys,json; print(json.load(sys.stdin)['slider']['width'])" 2>/dev/null)
+  if [ "${CURRENT_WIDTH:-0}" -gt 0 ]; then
+    sketchybar --animate tanh 20 --set volume_slider slider.width=0
+  else
+    sketchybar --animate tanh 20 --set volume_slider slider.width=80
+  fi
+}
 
-elif [ "$SENDER" = "mouse.clicked" ]; then
+if [ "$SENDER" = "volume_change" ]; then
+  VOLUME=$(osascript -e 'output volume of (get volume settings)')
+  set_icons "$VOLUME"
+
+elif [ "$SENDER" = "mouse.clicked" ] && [ "$NAME" = "volume" ]; then
+  toggle_slider
+
+elif [ "$SENDER" = "mouse.clicked" ] && [ "$NAME" = "volume_slider" ]; then
   osascript -e "set volume output volume $PERCENTAGE"
-  set_volume "$PERCENTAGE"
+  set_icons "$PERCENTAGE"
 
 elif [ "$SENDER" = "mouse.scrolled" ]; then
   CURRENT=$(osascript -e 'output volume of (get volume settings)')
@@ -30,5 +42,5 @@ elif [ "$SENDER" = "mouse.scrolled" ]; then
   [ "$NEW" -gt 100 ] && NEW=100
   [ "$NEW" -lt 0 ]   && NEW=0
   osascript -e "set volume output volume $NEW"
-  set_volume "$NEW"
+  set_icons "$NEW"
 fi

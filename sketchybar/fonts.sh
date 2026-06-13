e329d5be01be0cf633b2297cf9200859787fcae3
kaiqkt <kaiqkt@proton.me>
2026-06-13 16:23:31 -0300

    refactor(sketchybar): centralize fonts in fonts.sh

---
+++ sketchybar/fonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ICON_FONT="Hack Nerd Font:Regular:16.0"
+export LABEL_FONT="SF Pro:Semibold:15.0"
+export APP_ICON_FONT="sketchybar-app-font:Regular:16.0"
+export KNOB_FONT="Hack Nerd Font:Regular:10.0"
